0b9d1774c305cb54193f066ddf7bfcd5d8f4a551
Hrafn Eiríksson <hrafne@gmail.com>
2012-09-20 15:31:43 +0000

    EIDA* fully implemented and working.

--- SearchEnhancedIDAStar.java
+++ SearchEnhancedIDAStar.java
@@ -1,25 +1,26 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class SearchEnhancedIDAStar implements Search {
 
     // Private members
 
     private long nodesExpanded;
-    private Heuristic1 heuristic = new Heuristic1();  // TODO: Change!
+    private HashMap<Long, TransEntry> transTable = new HashMap<Long, TransEntry>();
 
     // Public members
 
     public boolean run( State s, Cost cost, ArrayList<Action> solution )
     {
-        heuristic.init(s);
+        transTable.clear();
         solution.clear();
         nodesExpanded = 0;
         boolean solved;
-        int threshold = heuristic.getHeuristic(s); // f(x) = g(x) + h(x) = 0 + h(x)
+        int threshold = s.getHeuristic(); // f(x) = g(x) + h(x) = 0 + h(x)
 
         while (true) {
-            IDAReturnValue ret = IDAStar(s, s.getStateID(), 0, threshold, cost, solution);
+            IDAReturnValue ret = EIDAStar(s, s.getStateID(), 0, threshold, cost, solution);
             solved = ret.solved;
             if (solved) {
                 Collections.reverse(solution);
@@ -32,8 +33,22 @@ public class SearchEnhancedIDAStar implements Search {
         }
     }
 
-    private IDAReturnValue IDAStar(State state, long stateId, int gScore, int threshold,
+    private IDAReturnValue EIDAStar(State state, long stateId, int gScore, int threshold,
                                    Cost cost, ArrayList<Action> solution) {
+
+        // Probes the transposition table.
+        if (transTable.containsKey(stateId)) {
+            TransEntry entry = transTable.get(stateId);
+            if (gScore > entry.gScore) {
+                return new IDAReturnValue(false, Integer.MAX_VALUE);
+            }
+            if (gScore == entry.gScore && threshold == entry.threshold) {
+                return new IDAReturnValue(false, Integer.MAX_VALUE);
+            }
+        }
+        TransEntry entry = new TransEntry(gScore, threshold);
+        transTable.put(stateId, entry);
+
         // Applies the state.
         state.setState(stateId);
 
@@ -48,16 +63,28 @@ public class SearchEnhancedIDAStar implements Search {
         nodesExpanded++;
         int nextThreshold = Integer.MAX_VALUE;
         for (Action action : actions) {
-            // Calculates f(x) = g(x) + h(x)
+
+            // g(x)
             int distToNeighbour = state.getCost(action);
             state.make(action);
             int neighbourGScore = gScore + distToNeighbour;
-            int neighbourFScore = neighbourGScore + heuristic.getHeuristic(state);
+            long neighbourStateId = state.getStateID();
 
+            // h(x): Probes the transposition table for a H-score, if not found: Calculate it.
+            int neighbourHScore;
+            if (transTable.containsKey(neighbourStateId)) {
+                TransEntry neighbourEntry = transTable.get(neighbourStateId);
+                neighbourHScore = neighbourEntry.hScore;
+            } else {
+                neighbourHScore = state.getHeuristic();
+            }
+
+            // f(x)
+            int neighbourFScore = neighbourGScore + neighbourHScore;
             if (neighbourFScore <= threshold) {
                 // Calls the search function recursively.
-                IDAReturnValue ret = IDAStar(
-                        state, state.getStateID(), neighbourGScore, threshold, cost, solution);
+                IDAReturnValue ret = EIDAStar(
+                        state, neighbourStateId, neighbourGScore, threshold, cost, solution);
                 if (ret.solved) {
                     solution.add(action);
                     cost.value += distToNeighbour;
@@ -71,11 +98,28 @@ public class SearchEnhancedIDAStar implements Search {
             state.retract(action);
         }
 
+        // Set the h(x) for this node in the transposition table.
+        entry.hScore = nextThreshold - gScore;
+
         return new IDAReturnValue(false, nextThreshold);
     }
 
     public long getNodesExpanded() {
         return nodesExpanded;
+    }
+
+}
+
+class TransEntry {
+
+    public int gScore;
+    public int hScore;
+    public int threshold;
+
+    TransEntry(int gScore, int threshold) {
+        this.gScore = gScore;
+        this.threshold = threshold;
+        this.hScore = 0; // TODO: this.
     }
 
 }
